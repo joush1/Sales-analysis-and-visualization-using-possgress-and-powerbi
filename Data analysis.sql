a1fe22892abd950c657ba4095d9a9df24aa50467
joush1 <111881271+joush1@users.noreply.github.com>
2024-07-21 20:36:14 +0100

    Update Data analysis.sql

--- Data analysis.sql
+++ Data analysis.sql
@@ -1,1 +1,11 @@
---sales
+create table project(
+"Transaction ID" integer,
+"Date" date,
+"Product Category" text,
+"Product Name" text,
+"Units Sold" integer,
+"Unit Price" decimal(10,2),
+"Total Revenue" decimal(10,2),
+"Region" text,
+"Payment Method" text
+);
